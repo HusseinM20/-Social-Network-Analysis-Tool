b38f7ae00e7f0f35947e50b26fefaa800bdc8b9d
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2024-07-10 16:07:42 +0300

    feat: add method to add edges to the graph

--- src/graph.py
+++ src/graph.py
@@ -5,7 +5,19 @@ class Graph:
 def add_vertex(self, vertex):
     if vertex not in self.adjacency_list:
         self.adjacency_list[vertex] = []
+        print("Vertex", vertex, "has been added!\n")
     else:
         print("Vertex already exists")
 
-       
+def add_edge(self, vertex1, vertex2):
+    if  vertex1 in self.adjacency_list and vertex2 in self.adjacency_list:
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            print("Edge between", vertex1, "and", vertex2, "has been added!\n")
+    elif vertex1 not in self.adjacency_list and vertex2 not in self.adjacency_list:
+      print("Vertex", vertex1, "and", vertex2, "do not exist")    
+    elif vertex1 not in self.adjacency_list:
+        print("Vertex", vertex1, "does not exist")
+    else:
+        print("Vertex", vertex2, "does not exist")    
+
