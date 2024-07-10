0a556f5a099a7e8de59cbcbfe4c8262b0dcec846
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2024-07-10 21:23:48 +0300

    feat: Add method to remove user from SocialNetwork class

--- src/socialNetwork.py
+++ src/socialNetwork.py
@@ -10,3 +10,13 @@ class SocialNetwork(Graph):
             print(f"{name} has been added to the social network.")
         else:
             print(f"{name} is already in the social network.")
+
+    def remove_user(self, user_id):
+        if user_id in self.users:
+            self.remove_vertex(user_id)
+            self.users.pop(user_id)
+            print(f"User {user_id} has been removed from the social network.")
+        else:
+            print(f"User {user_id} is not in the social network.")
+
+ 
