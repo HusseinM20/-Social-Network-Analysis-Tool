dd59dd4e0a96edbee29b16707cbae04d68ee1686
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2024-07-10 20:44:15 +0300

    Refactor SocialNetwork class to use super() for initialization

--- src/socialNetwork.py
+++ src/socialNetwork.py
@@ -1,6 +1,12 @@
 from graph import Graph
 class SocialNetwork(Graph):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.users = {}
     
+    def add_user(self, user_id, name):
+        if user_id not in self.users:
+            self.users[user_id] = User(user_id, name)
+            print(f"{name} has been added to the social network.")
+        else:
+            print(f"{name} is already in the social network.")
